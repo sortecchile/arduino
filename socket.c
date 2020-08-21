6870fc414eb3304b6af74f61cd5416a6f9bdaeac
Ricardo Silva <ricardo@loadingplay.com>
2020-08-20 23:06:43 -0400

    socket w/message

--- socket.c
+++ socket.c
@@ -12,38 +12,37 @@ using namespace websockets;
 
 void ConnectToWifi()
 {
-    Serial.println("Conectando al wifi");
-    WiFi.mode(WIFI_STA);
-    if (!WiFi.isConnected())
-        WiFi.begin(ssid, password);
+  Serial.println("Conectando al wifi");
+  WiFi.mode(WIFI_STA);
+  if (!WiFi.isConnected())
+    WiFi.begin(ssid, password);
 
-    unsigned long startAttemptTime = millis();
+  unsigned long startAttemptTime = millis();
 
-    while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < wifi_timeout)
-    {
-        Serial.print("...");
-        delay(100);
-    }
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < wifi_timeout)
+  {
+    Serial.print("...");
+    delay(100);
+  }
 
-    if (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.println("Fallido!!!");
-        ConnectToWifi();
-    }
-    else
-    {
-        Serial.print("conectado!!!");
-        Serial.println(WiFi.localIP());
-    }
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("Fallido!!!");
+    ConnectToWifi();
+  }
+  else
+  {
+    Serial.print("conectado!!!");
+    Serial.println(WiFi.localIP());
+  }
 }
 
 WebsocketsClient client;
 void setup()
 {
-    Serial.begin(115200);
-    ConnectToWifi();
+  Serial.begin(115200);
+  ConnectToWifi();
 }
-
 
 /**
  * ids
@@ -60,56 +59,66 @@ void setup()
   b5747c68
   b31bb419
  */
-void onPinToggle(id, _status)
+void onPinToggle(String id, String _status)
 {
-
 }
 
-
-void onMessageCallback(WebsocketsMessage message) {
-    Serial.print("Got Message: ");
-    Serial.println(message.data());
+void onMessageCallback(WebsocketsMessage message)
+{
+  Serial.print("Got Message: ");
+  Serial.println(message.data());
 }
 
-void onEventsCallback(WebsocketsEvent event, String data) {
-    if(event == WebsocketsEvent::ConnectionOpened) {
-        Serial.println("Connnection Opened");
-    } else if(event == WebsocketsEvent::ConnectionClosed) {
-        Serial.println("Connnection Closed");
-        is_connected = false;
-    } else if(event == WebsocketsEvent::GotPing) {
-        Serial.println("Got a Ping!");
-    } else if(event == WebsocketsEvent::GotPong) {
-        Serial.println("Got a Pong!");
-    }
+void onEventsCallback(WebsocketsEvent event, String data)
+{
+  if (event == WebsocketsEvent::ConnectionOpened)
+  {
+    Serial.println("Connnection Opened");
+  }
+  else if (event == WebsocketsEvent::ConnectionClosed)
+  {
+    Serial.println("Connnection Closed");
+    is_connected = false;
+  }
+  else if (event == WebsocketsEvent::GotPing)
+  {
+    Serial.println("Got a Ping!");
+  }
+  else if (event == WebsocketsEvent::GotPong)
+  {
+    Serial.println("Got a Pong!");
+  }
 }
-
 
 void connect()
 {
-    // Setup Callbacks
-    client.onMessage(onMessageCallback);
-    client.onEvent(onEventsCallback);
+  // Setup Callbacks
+  client.onMessage(onMessageCallback);
+  client.onEvent(onEventsCallback);
 
-    // Connect to server
-    if (!client.connect("ws://34.70.117.79:8765/uri"))
-    {
-        Serial.println("no connected");
-        delay(3000);
-        return;
-    }
+  // Connect to server
+  if (!client.connect("ws://34.70.117.79:8765/uri"))
+  {
+    Serial.println("no connected");
+    delay(3000);
+    return;
+  }
 
-    is_connected = true;
-    client.send("connect:ad804cb8");
-    client.send("connect:b0a5f4e2");
+  is_connected = true;
+  client.send("connect:ad804cb8");
+  client.send("connect:b0a5f4e2");
 
-    Serial.println("connected");
+  Serial.println("connected");
 }
 
 void loop()
 {
-    if (!is_connected)
-    {
-        connect();
-    }
+  if (!is_connected)
+  {
+    connect();
+  }
+  else
+  {
+    client.poll();
+  }
 }
